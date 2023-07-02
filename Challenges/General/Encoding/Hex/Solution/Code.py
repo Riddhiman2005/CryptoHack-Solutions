2549bb15d001108126386a86318eca279578011e
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2023-07-02 12:11:45 +0530

    Code

---
+++ Challenges/General/Encoding/Hex/Solution/Code.py
@@ -0,0 +1,9 @@
+hex_string = "63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d"
+
+# Convert hex string to bytes
+decoded_bytes = bytes.fromhex(hex_string)
+
+# Convert bytes to string
+flag = decoded_bytes.decode('utf-8')
+
+print(flag)
