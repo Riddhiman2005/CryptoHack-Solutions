6eefc609e004002245b7d12f9bc938f6b30c2411
Riddhi's Chaotic Universe <130882317+Riddhiman2005@users.noreply.github.com>
2023-07-03 09:49:47 +0530

    Code

---
+++ Challenges/Symmetric Ciphers/HOW AES WORKS/Diffusion Through Permutation/Solution/Code.py
@@ -0,0 +1,80 @@
+
+# The shift_rows function performs the ShiftRows operation in AES. It shifts the elements in the state matrix rows to the left, where the first row remains unchanged, the second row is shifted one position to the left, the third row is shifted 
+# two positions to the left, and the fourth row is shifted three positions to the left. 
+
+
+def shift_rows(s):
+    s[0][1], s[1][1], s[2][1], s[3][1] = s[1][1], s[2][1], s[3][1], s[0][1]
+    s[0][2], s[1][2], s[2][2], s[3][2] = s[2][2], s[3][2], s[0][2], s[1][2]
+    s[0][3], s[1][3], s[2][3], s[3][3] = s[3][3], s[0][3], s[1][3], s[2][3]
+
+# The inv_shift_rows function is the inverse operation of shift_rows. 
+# It reverses the shift performed in shift_rows, restoring the original state matrix. 
+
+def inv_shift_rows(s):
+    s[0][1], s[1][1], s[2][1], s[3][1] = s[3][1], s[0][1], s[1][1], s[2][1]
+    s[0][2], s[1][2], s[2][2], s[3][2] = s[2][2], s[3][2], s[0][2], s[1][2]
+    s[0][3], s[1][3], s[2][3], s[3][3] = s[1][3], s[2][3], s[3][3], s[0][3]
+
+# The mix_single_column function performs the MixColumns operation for a single column of the state matrix. 
+# It uses multiplication in the Rijndael's Galois field to ensure diffusion and non-linearity.
+
+
+def mix_single_column(a):
+    t = a[0] ^ a[1] ^ a[2] ^ a[3]
+    u = a[0]
+    a[0] ^= t ^ xtime(a[0] ^ a[1])
+    a[1] ^= t ^ xtime(a[1] ^ a[2])
+    a[2] ^= t ^ xtime(a[2] ^ a[3])
+    a[3] ^= t ^ xtime(a[3] ^ u)
+
+# The mix_columns function applies the mix_single_column operation to each column of the state matrix. 
+
+def inv_mix_columns(s):
+    for i in range(4):
+        u = xtime(xtime(s[i][0] ^ s[i][2]))
+        v = xtime(xtime(s[i][1] ^ s[i][3]))
+        s[i][0] ^= u
+        s[i][1] ^= v
+        s[i][2] ^= u
+        s[i][3] ^= v
+
+    mix_columns(s)
+
+
+# The inv_mix_columns function performs the inverse operation of mix_columns.
+# It reverses the mixing by applying inverse transformations to each column of the state matrix.
+
+def inv_mix_columns(s):
+    for i in range(4):
+        u = xtime(xtime(s[i][0] ^ s[i][2]))
+        v = xtime(xtime(s[i][1] ^ s[i][3]))
+        s[i][0] ^= u
+        s[i][1] ^= v
+        s[i][2] ^= u
+        s[i][3] ^= v
+
+    mix_columns(s)
+
+
+
+
+state = [
+    [108, 106, 71, 86],
+    [96, 62, 38, 72],
+    [42, 184, 92, 209],
+    [94, 79, 8, 54],
+]
+
+inv_mix_columns(state)
+inv_shift_rows(state)
+
+result = []
+for row in state:
+    result.extend(row)
+
+flag = bytes(result)
+print(flag)
+
+
+
